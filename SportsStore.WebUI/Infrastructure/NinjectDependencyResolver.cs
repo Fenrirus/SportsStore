d99a1fac4dac56ce2456e2f2dd8fc8f58723978a
Marian Marian <fenrir55@gmail.com>
2020-08-12 21:39:13 +0200

    Page configuration

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿namespace SportsStore.WebUI.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Moq;
+    using Ninject;
+    using SportsStore.Domain.Abstract;
+    using SportsStore.Domain.Entities;
+
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>{
+                new Product {Name = "Piłka nożna", Price = 25},
+                new Product {Name = "Deska surfingowa", Price = 180},
+                new Product {Name = "But do biegania", Price = 95},
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
