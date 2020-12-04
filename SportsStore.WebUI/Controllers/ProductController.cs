58c99b3c785fd0235342fdd7298d269b1c4c56cd
Marian Marian <fenrir55@gmail.com>
2020-12-04 14:25:27 +0100

    Stronnicowanie plus test

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,20 +1,22 @@
 ﻿using SportsStore.Domain.Abstract;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(o => o.ProductId).Skip((page - 1) * PageSize).Take(PageSize));
         }
     }
 }
