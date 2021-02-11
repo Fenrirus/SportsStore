b08da0cb293f417431c059aee0d42a031225f1c6
Marian Marian <fenrir55@gmail.com>
2021-02-11 20:01:03 +0100

    Dodawanie Numeracji Stron

--- SportsStore.UnitTests/Test.cs
+++ SportsStore.UnitTests/Test.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.HtmlHelpers;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class Test
     {
         [TestMethod]
         public void CanBePaginate()
@@ -35,6 +38,27 @@ namespace SportsStore.UnitTests
             Assert.IsTrue(arrayProduct.Length == 2);
             Assert.AreEqual(arrayProduct[0].Name, "P4");
             Assert.AreEqual(arrayProduct[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_links()
+        {
+            HtmlHelper myHelper = null;
+
+            PagingInfo pagingInfo = new PagingInfo()
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10,
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
+
+            var results = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+            Assert.AreEqual(@"<a class=""btn btn-outline-secondary"" href=""Strona1"">1</a>"
+                          + @"<a class=""btn btn-outline-secondary btn-primary selected"" href=""Strona2"">2</a>"
+                          + @"<a class=""btn btn-outline-secondary"" href=""Strona3"">3</a>"
+                          , results.ToString());
         }
     }
 }
